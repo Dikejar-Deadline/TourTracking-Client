7446f3017ea4a05e3b56e85858657d0f9fe06b6b
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2022-10-01 08:40:45 +0800

    Setup socket.io

--- tourtracking/pages/index.js
+++ tourtracking/pages/index.js
@@ -1,9 +1,31 @@
 import "mapbox-gl/dist/mapbox-gl.css";
-import Fav from "./components/Fav";
 import MapTrack from "./components/maps/MapTrack";
+import socketIO from "socket.io-client";
+import { useEffect } from "react";
+
+const socket = socketIO.connect("http://localhost:4000");
 
 function Home() {
-  return <MapTrack />;
+  useEffect(() => {
+    socket.on("messageResponse", (data) => console.log(data));
+  }, []);
+
+  return (
+    <>
+      <MapTrack />;
+      <button
+        onClick={() => {
+          socket.emit("message", {
+            text: "message",
+            name: "userName",
+            socketID: socket.id,
+          });
+        }}
+      >
+        Emit message
+      </button>
+    </>
+  );
   // return <Fav />;
 }
 
